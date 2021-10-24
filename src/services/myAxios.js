272e55f8fcf2a5698caf40d328fd1eef16fba0b7
yuraoliynik <86535420+yuraoliynik@users.noreply.github.com>
2021-10-24 17:33:26 +0300

    Update myAxios.js

--- src/services/myAxios.js
+++ src/services/myAxios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const config = {
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
-        authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzdhOTdiNmU3NjU2NDYxM2ViM2YzNjdhZWI2ZmU3NSIsInN1YiI6IjYxMmY3NjYwYzFmZmJkMDAyYjk4OGU1OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W5vq4qcn2PM92It4VnliPopuFxLSwazxmlmttUgKANA'
+        authorization: 
     }
 }
 
